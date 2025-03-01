5c38fd98e9a0c0d154a34e7bcb87591dfad39610
Vinn <kevgun03@gmail.com>
2025-03-01 20:20:13 +0700

    Initial Commit

---
+++ fixproTests/fixproTests.swift
@@ -0,0 +1,17 @@
+//
+//  fixproTests.swift
+//  fixproTests
+//
+//  Created by Vin on 01/03/25.
+//
+
+import Testing
+@testable import fixpro
+
+struct fixproTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ fixproUITests/fixproUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  fixproUITestsLaunchTests.swift
+//  fixproUITests
+//
+//  Created by Vin on 01/03/25.
+//
+
+import XCTest
+
+final class fixproUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
