16c30a166d08443818195572b4f6af4daa9f2387
Vinn <kevgun03@gmail.com>
2025-04-10 19:11:24 +0700

    Initial Commit

---
+++ fixpro-mobileTests/fixpro_mobileTests.swift
@@ -0,0 +1,17 @@
+//
+//  fixpro_mobileTests.swift
+//  fixpro-mobileTests
+//
+//  Created by Vin on 10/04/25.
+//
+
+import Testing
+@testable import fixpro_mobile
+
+struct fixpro_mobileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
